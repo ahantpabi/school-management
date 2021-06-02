750b0397b63a75e07b91b56b2fdd91c4fd57b2b2
Pabitra Muni <mailtopabi@gmail.com>
2021-06-02 15:09:16 +0530

    Initial Commit

---
+++ src/test/java/com/github/ahant/schoolmanagement/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ahant.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
